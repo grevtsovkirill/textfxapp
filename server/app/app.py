46ffd43ed678876deedf9336f98166635672d781
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-10-30 13:24:29 +0100

    add get post methods

--- server/app/app.py
+++ server/app/app.py
@@ -1,13 +1,30 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
+# configuration
+DEBUG = True
 
-# create and initialize a new Flask app
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+TXT = [{
+        'in_text': 'place text here'
+    }]
 
 
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def hello():
-    return "Hello, World!"
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        TXT.append({'in_text': post_data.get('in_text')})
+    else:
+        response_object['msg'] = TXT
+    return jsonify(response_object)
+    # return "Hello, World!"
 
 
 if __name__ == "__main__":
