4ee8e19a3102690c20e868ea867fc4b4f9b76247
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-10-30 14:51:38 +0100

    minimal text entry interface

--- client/src/components/TextFx.vue
+++ client/src/components/TextFx.vue
@@ -1,17 +1,36 @@
 <template>
   <div class="container">
-    <button type="button" class="btn btn-primary">{{ msg }}</button>
+   <div class="row">
+      <div class="col-sm-10">
+    <button type="button" class="btn btn-primary">{{ msg1 }}</button>
+    <br>
+    <table class="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Text</th>
+              <th scope="col">New text</th>
+            </tr>
+          </thead>
+          <tbody>
+
+            <td> <input v-model="in_message"> </td>
+            <td> {{in_message}} </td>
+          </tbody>
+    </table>
+    </div>
+  </div>
   </div>
 </template>
 
 <script>
-//  import axios from 'axios';
+// import axios from 'axios';
 
 export default {
   name: 'TextFx',
   data() {
     return {
-      msg: 'Text',
+      msg1: 'Text',
+      in_message: '',
     };
   },
 };
