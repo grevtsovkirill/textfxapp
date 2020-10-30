8c82a3575581fe84aee28f82fb533c311bf4dba1
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-10-30 11:32:40 +0100

    initial commit of backend - minimal hello world pack with test

---
+++ server/tests/app_test.py
@@ -0,0 +1,13 @@
+from app.app import app
+
+
+def test_index():
+    tester = app.test_client()
+    response = tester.get("/", content_type="html/text")
+
+    assert response.status_code == 200
+    assert response.data == b"Hello, World!"
+
+
+if __name__ == '__main__':
+    unittest.main()
