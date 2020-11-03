e891d292c4a59f6e3ed9b46098cb7580e3ded41c
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-11-03 21:35:24 +0100

    combine get and post methods to read data from fronend

--- server/app/app.py
+++ server/app/app.py
@@ -7,16 +7,23 @@ DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-# enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
+VAR = []
 
 @app.route("/", methods=['GET','POST'])
 def hello():
-    data = request.data
-    return data
-
-    # return "Hello, World!"
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        data = request.get_data()
+        msg = data.decode("utf-8")
+        var = msg + " - 1"
+        print(var)
+        response_object['txt'] = var
+    else:
+        response_object['txt'] = '0'
+    print(response_object)
+    return response_object
 
 
 if __name__ == "__main__":
